b40c6ef27f08ff22714ce52c148df28261bac26b
edmundo papi <edmundopierre@yahoo.com>
2020-09-23 02:26:03 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,31 +1,3 @@
 class PigLatinizer
 
-attr_reader :text
-
-def initilization(text)
-
-     @text =text
-end
-
-
-def piglatinize(word)
-    text_split = word.split(" ")
-    text_split.each do |t|
-
-   if t[0]== 'e' ||t[0]== 'i' ||t[0]== 'u' ||t[0]== 'o' ||t[0]== 'a'
-     vowel = t +'way'
-     puts "#{vowel}"
-#   elsif index[/\A[bcdfghjklmnpqrstvwxyz]*/i].length == 2
-#     con = index + index[0..1]+ 'way'
-#     con_length_2 = con.sub(pr[0..1],'')
-#     puts "#{con_length_2}"
-#
-#   elsif index[/\A[bcdfghjklmnpqrstvwxyz]*/i].length == 1
-#
-#     con = index + index[0]+ 'way'
-#     con_length_1 = con.sub(pr[0],'')
-#     puts "#{con_length_1}"
-  end
- end
-end
 end
