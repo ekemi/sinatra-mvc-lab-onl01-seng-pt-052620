77516f9c8f6a6d34607a5232f2859371b209e4c4
edmundo papi <edmundopierre@yahoo.com>
2020-09-23 04:01:34 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,3 +1,18 @@
 class PigLatinizer
 
+
+  def translate_word(text)
+  
+    if /^[aeiou]/i.match(text)
+      puts "#{text}way"
+    else  
+      parts = text.split(/([aeiou].*)/)
+      puts "#{parts[1]}#{parts[0]}ay"
+    end
+  end
+  
+  def pi (text)
+    word = text.split(' ')
+    word.map{|word| translate_word(word)}.join(' ') 
+  end
 end
