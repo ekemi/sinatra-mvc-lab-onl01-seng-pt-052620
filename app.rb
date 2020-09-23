2e5dfc561581ff5695ed7cb436e0a446cfd44189
edmundo papi <edmundopierre@yahoo.com>
2020-09-23 04:12:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,14 +4,13 @@ class App < Sinatra::Base
 
   get '/' do
    erb :user_input
-  end
+ end
 
-  post '/piglatinize' do
-    @user = PigLatinizer.params[:text]
-  end
-  post '/piglatinize' do
-    @user = PigLatinizer.params[:text]
-  end
+ post '/piglatinize' do
+   phrase = PigLatinizer.new
+   @user_input = phrase.to_pig_latin(params[:user_phrase])
+   erb :piglatinize
+ end
 
 
 end
